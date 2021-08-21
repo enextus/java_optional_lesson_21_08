9d373eb7d7da3c5c24677d98fb0dbfdcfd6f007c
enextus <enextus@gmail.com>
2021-08-21 15:23:05 +0200

    21_08_21__

---
+++ 21_08_20_unittesting/src/Problem2Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem2Test {
+
+    @BeforeEach
+    void setUp() {
+        Problem2 pr2 = new Problem2();
+    }
+
+    @Test
+    void method() {
+    }
+}
