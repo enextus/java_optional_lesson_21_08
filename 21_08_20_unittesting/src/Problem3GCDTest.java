af3b411426a2ae521a38cbf57220887749555fa7
enextus <enextus@gmail.com>
2021-08-21 17:02:16 +0200

    21_08_21__

--- 21_08_20_unittesting/src/Problem3GCDTest.java
+++ 21_08_20_unittesting/src/Problem3GCDTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Problem3GCDTest {
 
     Problem3GCD gcdSolver;
@@ -11,6 +13,28 @@ class Problem3GCDTest {
     }
 
     @Test
-    void gcdSolver() {
+    public void testGcd_nEquals15_20_5() {
+        assertEquals(5, gcdSolver.gcdSolver(15, 20));
     }
+
+    @Test
+    public void testGcd_nEquals20_15_5() {
+        assertEquals(5, gcdSolver.gcdSolver(20, 15));
+    }
+
+    @Test
+    public void test_nEquals1071_462_21() {
+        assertEquals(21, gcdSolver.gcdSolver(1071, 462));
+    }
+
+    @Test
+    public void test_nEquals462_1071_21() {
+        assertEquals(21, gcdSolver.gcdSolver(462, 1071));
+    }
+
+    @Test
+    public void test_nEquals100045_1071_21() {
+        assertEquals(17, gcdSolver.gcdSolver(100045, 1071));
+    }
+
 }
