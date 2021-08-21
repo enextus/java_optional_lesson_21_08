8d66400bec2d9e0e640a85bf7d8355a4d0e927f9
enextus <enextus@gmail.com>
2021-08-21 17:33:04 +0200

    21_08_21__done

--- 21_08_20_unittesting/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Problem2 pr2 = new Problem2();
-
-        System.out.println(pr2.method("AaB!S§$%rtFgr1245Qa."));
-    }
-}
